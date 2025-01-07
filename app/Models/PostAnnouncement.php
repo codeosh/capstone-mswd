0d99efd2d2350c73875970dd1d481988d2f77307
codeosh <derekcatigan@gmail.com>
2025-01-08 00:15:26 +0800

    post announcement

---
+++ app/Models/PostAnnouncement.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PostAnnouncement extends Model
+{
+    use HasFactory;
+
+    protected $table = 'announcements';
+
+    protected $fillable = [
+        'user_id',
+        'caption',
+        'media_file',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Http\Controllers\Page\PostAnnouncement;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -45,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function announcements()
+    {
+        return $this->hasMany(PostAnnouncement::class);
     }
 }
